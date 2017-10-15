e50ba5c4158c292a2917ec778696163277d68019
nancy0625 <1308013168@qq.com>
2017-10-15 21:41:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/taskmanager/Constants.java
@@ -0,0 +1,14 @@
+package com.example.asus.taskmanager;
+
+/**
+ * Created by asus on 2017/5/4.
+ */
+public class Constants {
+
+    public static final String ACCOUNT_TYPE="com.example.asus.taskmanager";
+
+    public static final String AUTHTOKEN_TYPE="com.example.asus.taskmanager";
+
+
+    public static final String BASE_URL="http://192.168.1.104/task_manager/v1";
+}
---
+++ app/src/main/java/com/example/asus/taskmanager/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.asus.taskmanager;
+
+import android.accounts.AccountAuthenticatorActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView welcomeImg;
+    private Animation anim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        welcomeImg = (ImageView)findViewById(R.id.welcome);
+        anim=new AlphaAnimation(0.1f,1.0f);//设置透明度动画效果
+        anim.setDuration(200);//设置动画持续时间2秒钟
+        welcomeImg.startAnimation(anim);
+        anim.setAnimationListener(new Animation.AnimationListener(){
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //这里启动注册和登录页面，目前什么都不做，直接退出
+               /* Intent intent = new Intent(MainActivity.this, AccountAuthenticatorActivity.class);
+                startActivity(intent);
+                */
+                finish();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
